cc4e8976d53ae1bbf37908c3dcda7ef493b87fab
dotnet31102018 <44984976+dotnet31102018@users.noreply.github.com>
2019-07-10 18:36:54 +0300

    Add files via upload

---
+++ WriteToFileAsync/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hw1007async
+{
+    class Program
+    {
+        public const string FILE_PATH = @"c:\itay\kovetz.txt";
+        public const int RANGE_WRITE = 1000;
+        public const int DELAY_MS = 100;
+        private async static Task WriteToFileAsync()
+        {
+            StreamWriter sw = new StreamWriter(FILE_PATH);
+            for (int i = 1; i <= RANGE_WRITE; i++)
+            {
+                await sw.WriteAsync(i.ToString() + " ");
+                Thread.Sleep(10);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ready. Wait 5 seconds ... very busy now");
+            Task t = WriteToFileAsync();
+            while (!t.Wait(10))
+            {
+                Console.Write(".");
+            }
+
+            Console.WriteLine("Done!!!");
+        }
+    }
+}
